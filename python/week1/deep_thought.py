f5a2fb6d1d4e039ea5a13daeda1e716424b33739
Joan Njeri <joannjeri2015@gmail.com>
2026-06-19 13:11:07 +0300

    Add deep_thought.py

---
+++ python/week1/deep_thought.py
@@ -0,0 +1,5 @@
+answer = input("What is the Answer to the Great Question of Life, The Universe, and Everything? ")
+if answer.strip() == "42" or answer.strip() == "0":
+    print("Yes")
+else: 
+    print("No")
