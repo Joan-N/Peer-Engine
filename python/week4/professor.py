1674f476eb47943a0b36d211529e4262ce6ec286
Joan Njeri <joannjeri2015@gmail.com>
2026-06-19 21:56:52 +0300

    Add professor.py

---
+++ python/week4/professor.py
@@ -0,0 +1,44 @@
+import random
+
+def main():
+    level = get_level()
+    score = 0
+    for _ in range(10):
+        x = generate_integer(level)
+        y = generate_integer(level)
+        answer = x + y
+        tries = 0
+        while tries < 3:
+            try:
+                guess = int(input(f"{x} + {y} = "))
+                if guess == answer:
+                    score += 1
+                    break
+                else:
+                    print("EEE")
+                    tries += 1
+            except ValueError:
+                print("EEE")
+                tries += 1
+        if tries == 3:
+            print(f"{x} + {y} = {answer}")
+    print(f"Score: {score}")
+
+def get_level():
+    while True:
+        try:
+            level = int(input("Level: "))
+            if level in [1, 2, 3]:
+                return level
+        except ValueError:
+            pass
+
+def generate_integer(level):
+    if level == 1:
+        return random.randint(0, 9)
+    elif level == 2:
+        return random.randint(10, 99)
+    else:
+        return random.randint(100, 999)
+    
+main()
