f71dfb04840fa812dc86dd73b11c60f476d001be
Joan Njeri <joannjeri2015@gmail.com>
2026-06-19 12:48:34 +0300

    Add outdated.py

---
+++ python/week3/outdated.py
@@ -0,0 +1,24 @@
+month = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+
+]
+while True:
+    try:
+        date = input("Date: ")
+        if "/" in date:
+            month, day, year = date.split("/")
+            month = int(month)
+            day = int(day)
+            year = int(year)
+        else:
+            month_name, day, year = date.split(" ")
+            day = day.rstrip(",")
+            month = month.index(month_name) + 1
+            day = int(day)
+            year = int(year)
+        if 1 <= month <= 12 and 1 <= day <= 31:
+            print(f"{year:04}-{month:02}-{day:02}")  
+            break
+    except (ValueError, IndexError):
+        pass
